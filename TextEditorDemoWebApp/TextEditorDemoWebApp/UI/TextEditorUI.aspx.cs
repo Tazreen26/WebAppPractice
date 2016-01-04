9aa8705957e962242c53e0385c10affa1dc09cff
furiousNoob <al.imran.04.120@gmail.com>
2016-01-04 21:53:48 +0600

    RichTextBoxDemoWebApp is created

---
+++ TextEditorDemoWebApp/TextEditorDemoWebApp/UI/TextEditorUI.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TextEditorDemoWebApp.UI {
+    public partial class TextEditorUI : System.Web.UI.Page {
+        protected void Page_Load( object sender, EventArgs e ) {
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e) {
+            lblDisplay.Visible = true;
+            RichTextBox.Visible = false;
+            lblDisplay.Text = RichTextBox.Text;
+        }
+
+        protected void btnCancel_Click(object sender, EventArgs e) {
+            
+        }
+    }
+}
